638340185287a3f111982454cdc450bae5733f00
IzaacFacundo <izaacfacundo@utexas.edu>
2023-04-20 04:29:26 +0000

    added multiple functions to worker.py

--- src/worker.py
+++ src/worker.py
@@ -1,5 +1,6 @@
 import csv
 from hotqueue import HotQueue
+import matplotlib as plt
 
 data = {}
 data['launches'] = []
@@ -11,7 +12,7 @@ with open('mission_launches.csv', 'r') as f:
 
 # q = HotQueue("queue", host="10.233.38.133", port=6379, db=1)
 
-def return_rocket_names_by_org(org_name):
+def get_rocket_names_by_org(org_name:str) -> list:
     rocket_names = []
     for item in data['launches']:
         if item['Organisation'] == org_name:
@@ -19,4 +20,32 @@ def return_rocket_names_by_org(org_name):
 
     return(rocket_names)
 
-print(return_rocket_names_by_org('Roscosmos'))
+def get_total_cost_for_org(org_name:str) -> float:
+    cost = 0
+    for item in data['launches']:
+        if item['Organisation'] == org_name:
+            cost += item['Price']
+
+    return(cost) # cost is in millions
+
+def get_success_rate_for_org(org_name:str) -> float:
+    total_launches = 0
+    successful_launches = 0
+    for item in data['launches']:
+        if item['Organisation'] == org_name:
+            total_launces += 1
+            if item['Mission_Status'] == 'Success':
+                successful_launches += 1
+    success_rate = success_launches / total_launches
+
+    return(success_rate)
+
+def list_active_rockets() -> list:
+    active_rockets = []
+    for item in data['launches']:
+        if item['Rocket_Status'] == 'StatusActive':
+            active_rockets.append(item['Detail'][:item['Detail'].index(" |")])
+    active_rockets = list(set(active_rockets)) # remove duplicates from list
+    return(active_rockets)
+
+print(list_active_rockets())
