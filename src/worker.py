6ea3ad1580301ed397fe218f5334b0bf054f740f
Steven Oh <91847206+jaeestee@users.noreply.github.com>
2023-04-26 17:25:18 -0500

    Update worker.py

--- src/worker.py
+++ src/worker.py
@@ -4,6 +4,14 @@ from redis import Redis
 from jobs import *
 import json
 
+redis_ip = os.environ.get('REDIS_IP')
+if not redis_ip:
+    raise Exception()
+
+rd = Redis(host = redis_ip, port=6379, db=0)
+q = HotQueue('queue', host = redis_ip, port = 6379, db=1)
+rd2 = Redis(host = redis_ip, port=6379, db=2)
+
 @q.worker
 def execute_job(item: str) -> dict:
     """
