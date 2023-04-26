c75619ac46b1809fa72ac733d68adeb88df3e567
acc4476 <asher.curaportillo@austin.utexas.edu>
2023-04-26 15:36:40 -0500

    deleting redis stuff

--- src/worker.py
+++ src/worker.py
@@ -3,14 +3,6 @@ from geopy.geocoders import Nominatim
 from redis import Redis
 from jobs import *
 
-redis_ip = os.environ.get('REDIS_IP')
-if not redis_ip:
-    raise Exception()
-
-rd = Redis(host = redis_ip, port=6379, db=0)
-q = HotQueue('queue', host = redis_ip, port = 6379, db=1)
-rd2 = Redis(host = redis_ip, port=6379, db=2)
-
 @q.worker
 def execute_job(item: str) -> dict:
     """
