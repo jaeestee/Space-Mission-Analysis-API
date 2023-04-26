906711c1d3d343c90a5d4806806cb2bb65d24cb6
acc4476 <asher.curaportillo@austin.utexas.edu>
2023-04-26 15:25:03 -0500

    testing the redis error

--- src/worker.py
+++ src/worker.py
@@ -3,6 +3,14 @@ from geopy.geocoders import Nominatim
 from redis import Redis
 from jobs import *
 
+redis_ip = os.environ.get('REDIS_IP')
+if not redis_ip:
+    raise Exception()
+
+rd = redis.Redis(host = redis_ip, port=6379, db=0)
+q = hotqueue.HotQueue('queue', host = redis_ip, port = 6379, db=1)
+rd2 = redis.Redis(host = redis_ip, port=6379, db=2)
+
 @q.worker
 def execute_job(item: str) -> dict:
     """
